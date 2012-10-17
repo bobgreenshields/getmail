1241d449a8a13bb62f1d4a02bcfb41f21cdd69f9
Robert Greenshields <bob@thegreenshields.com>
2012-10-17 12:35:01 +0100

    try to get rescue block right

--- lib/getmail.rb
+++ lib/getmail.rb
@@ -59,18 +59,17 @@ lock_file = config[:lock_file]
 if File.exist? stop_file
   logger.info { "Stop file found, mail will NOT be checked" }
 else
-	begin
-		Lockfile.new(lock_file, :retries => 0) do
-			logger.info { "Checking email" }
-			logger.debug { "calling #{getmail_call}" }
-			res = `#{getmail_call}`
-			if $?.to_i == 0
-				logger.info { res }
-			else
-				logger. error { res }
-			end
+	Lockfile.new(lock_file, :retries => 0) do
+		logger.info { "Checking email" }
+		logger.debug { "calling #{getmail_call}" }
+		res = `#{getmail_call}`
+		if $?.to_i == 0
+			logger.info { res }
+		else
+			logger. error { res }
+		end
 		rescue Lockfile::MaxTriesLockError => e
 			logger.info { "Another fetcher is running" }
-	end
-end
+	end # Lockfile... do
+end # else
 
